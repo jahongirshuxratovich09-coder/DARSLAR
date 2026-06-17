8992cd78e59a74e740c59a029bcf593f04451b2d
Пользователь <jahongirsshuxratovich09@gmail.com>
2026-06-17 17:10:17 +0500

    SALOM MEN TXT PP

---
+++ fizbuzz.py
@@ -0,0 +1,23 @@
+#for son in range(1, 101):
+
+  #  if son % 3 == 0 and son % 5 == 0:
+  #      print("FizzBuzz")
+
+ #   elif son % 3 == 0:
+ #       print("Fizz")
+
+ #   elif son % 5 == 0:
+ #       print("Buzz")
+
+ #   else:
+#        print(son)
+
+for son in range(1,1001):
+    if son % 3 == 0 and son % 5 ==0:
+        print("FizzBuzz")
+    elif son % 3 ==0:
+        print("Fizz")
+    elif son % 5 ==0:
+        print("Buzz")
+    else:
+        print(son)
